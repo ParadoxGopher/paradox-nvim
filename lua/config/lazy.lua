62e7b305ed7ed34738bf0daa46cdefce0759e463
Gerhard <7468413+ParadoxGopher@users.noreply.github.com>
2025-01-01 12:12:21 +0100

    init

---
+++ lua/config/lazy.lua
@@ -0,0 +1,24 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- auto-install lazy plugin manager
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	print("installing lazy plugin manager")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+	print("done.")
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	spec = {
+		-- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "plugins" },
+	},
+})
